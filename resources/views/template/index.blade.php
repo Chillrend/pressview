42bd0c1d72659a76732984360f06252ad2eba3a5
unknown <madjavacoder@gmail.com>
2021-12-02 08:36:01 +0700

    fixed links

--- resources/views/template/index.blade.php
+++ resources/views/template/index.blade.php
@@ -20,9 +20,9 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ml-auto">
-                        <li class="nav-item"><a class="nav-link" href="#!">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Statistik</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">List Buku</a></li>
+                        <li class="nav-item"><a class="nav-link" href="http://press.pnj.ac.id/">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{url('/chart')}}">Statistik</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{url('/table')}}">List Buku</a></li>
                     </ul>
                 </div>
             </div>
